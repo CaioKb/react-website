d9a3056ecb38a210d8d4b4621666646f61df0fb8
caiokb <caiokbrasil@gmail.com>
2021-04-14 17:54:00 -0300

    Routes correction

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 
 import Login from '../containers/Login/Login.js'
 import Home from '../containers/Home/Home'
 
 export default function Routes() {
-    return(
-        <Switch>
-            <Redirect exact path='/' to='/login'/>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/home" component={Home}/>
-        </Switch>
-        
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Redirect exact path='/' to='/login' />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/home" component={Home} />
+            </Switch>
+        </BrowserRouter>
     );
 }
